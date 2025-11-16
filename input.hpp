b84964ee3061bf4111a92a8f8cb95e1474e13ea4
Mohamed Khaled <muhammad-khaled-tech>
2025-11-16 15:12:57 +0200

    Initial commit

---
+++ input.hpp
@@ -0,0 +1,12 @@
+#pragma once
+enum KeyType {
+    KEY_UP,       
+    KEY_DOWN,     
+    KEY_HOME,     
+    KEY_END,      
+    KEY_ENTER,    
+    KEY_ESCAPE,   
+    KEY_QUIT,     
+    KEY_UNKNOWN   
+};
+KeyType readKey();
---
+++ ui.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+
+void drawLoadingBar(int percent);
+void printMenu(int selectedOption);
+void openScreen(int selectedOption);
+int getTotalOptions(); 
