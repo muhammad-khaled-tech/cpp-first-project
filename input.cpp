44b43ca06b2645da66b0d0edce68aac027f14183
Mohamed Khaled <muhammad-khaled-tech>
2025-11-16 20:11:29 +0200

    updates for windows

--- input.cpp
+++ input.cpp
@@ -1,8 +1,45 @@
-#include "input.hpp" 
-#include <stdio.h>   
-#include <unistd.h>  
-#include <termios.h> 
+#include "input.hpp"
+#include "terminal.hpp"
 
+#ifdef _WIN32
+    #include <conio.h>
+#else
+    #include <stdio.h>
+    #include <unistd.h>
+#endif
+
+#ifdef _WIN32
+
+KeyType readKey() {
+    if (_kbhit()) {
+        char c = _getch();
+        if (c == 224) { 
+            char c2 = _getch();
+            switch (c2) {
+                case 72: return KEY_UP;
+                case 80: return KEY_DOWN;
+                case 71: return KEY_HOME;
+                case 79: return KEY_END;
+                default: return KEY_UNKNOWN;
+            }
+        }
+        if (c == '\r') {
+            return KEY_ENTER;
+        }
+        if (c == 27) { 
+            return KEY_ESCAPE;
+        }
+        if (c == 'q') {
+            return KEY_QUIT;
+        }
+        return KEY_UNKNOWN;
+    
+    } else {
+        return KEY_UNKNOWN;
+    }
+}
+
+#else
 
 KeyType readKey() {
     int nread;
@@ -10,46 +47,91 @@ KeyType readKey() {
     nread = read(STDIN_FILENO, &c, 1);
 
     if (nread <= 0) {
-        return KEY_UNKNOWN; 
+        return KEY_UNKNOWN;
     }
 
     if (c == '\033') {
-        char seq[3]; 
- 
-        if (read(STDIN_FILENO, &seq[0], 1) == 0) {
-     
-            return KEY_ESCAPE;
-        }
-        
-   
-        if (read(STDIN_FILENO, &seq[1], 1) == 0) {
-      
-            return KEY_UNKNOWN;
-        }
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) == 0) return KEY_ESCAPE;
+        if (read(STDIN_FILENO, &seq[1], 1) == 0) return KEY_UNKNOWN;
 
-    
         if (seq[0] == '[') {
             switch (seq[1]) {
                 case 'A': return KEY_UP;
                 case 'B': return KEY_DOWN;
-                case 'H': return KEY_HOME; 
-                case 'F': return KEY_END;  
+                case 'H': return KEY_HOME;
+                case 'F': return KEY_END;
             }
         }
-        
-   
         return KEY_UNKNOWN;
     }
 
-
-    if (c == '\n' || c == '\r') {
+    if (c == '\n') {
         return KEY_ENTER;
     }
-
     if (c == 'q') {
         return KEY_QUIT;
     }
 
-
     return KEY_UNKNOWN;
 }
+
+#endif
+
+
+
+// #include "input.hpp" 
+// #include <stdio.h>   
+// #include <unistd.h>  
+// #include <termios.h> 
+
+
+// KeyType readKey() {
+//     int nread;
+//     char c;
+//     nread = read(STDIN_FILENO, &c, 1);
+
+//     if (nread <= 0) {
+//         return KEY_UNKNOWN; 
+//     }
+
+//     if (c == '\033') {
+//         char seq[3]; 
+ 
+//         if (read(STDIN_FILENO, &seq[0], 1) == 0) {
+     
+//             return KEY_ESCAPE;
+//         }
+        
+   
+//         if (read(STDIN_FILENO, &seq[1], 1) == 0) {
+      
+//             return KEY_UNKNOWN;
+//         }
+
+    
+//         if (seq[0] == '[') {
+//             switch (seq[1]) {
+//                 case 'A': return KEY_UP;
+//                 case 'B': return KEY_DOWN;
+//                 case 'H': return KEY_HOME; 
+//                 case 'F': return KEY_END;  
+//             }
+//         }
+        
+   
+//         return KEY_UNKNOWN;
+//     }
+
+
+//     if (c == '\n' || c == '\r') {
+//         return KEY_ENTER;
+//     }
+
+//     if (c == 'q') {
+//         return KEY_QUIT;
+//     }
+
+
+//     return KEY_UNKNOWN;
+// }
--- terminal.cpp
+++ terminal.cpp
@@ -1,23 +1,83 @@
-#include "terminal.hpp" 
-#include <termios.h>
-#include <unistd.h>
-#include <stdlib.h>
+// #include "terminal.hpp" 
+// #include <termios.h>
+// #include <unistd.h>
+// #include <stdlib.h>
+// #include <iostream>
+// #include "ansi_terminal.hpp" 
+
+// using namespace Ansi;
+// using namespace std;
+
+// static struct termios defaultMode;
+// void disableRawMode()
+// {
+
+//     tcsetattr(STDIN_FILENO, TCSANOW, &defaultMode);
+    
+
+//     cout << CLEAR_SCREEN << CURSOR_HOME << RESET << SHOW_CURSOR << flush;
+// }
+
+
+// void enableRawMode()
+// {
+//     tcgetattr(STDIN_FILENO, &defaultMode);
+//     atexit(disableRawMode);
+//     struct termios rawMode = defaultMode;
+//     rawMode.c_lflag &= ~(ICANON | ECHO | ISIG);
+//     rawMode.c_cc[VMIN] = 0;
+//     rawMode.c_cc[VTIME] = 1;
+//     tcsetattr(STDIN_FILENO, TCSANOW, &rawMode);
+// }
+
+#include "terminal.hpp"
 #include <iostream>
-#include "ansi_terminal.hpp" 
+#include "ansi_terminal.hpp"
 
 using namespace Ansi;
 using namespace std;
 
-static struct termios defaultMode;
+#ifdef _WIN32
+
+#include <windows.h>
+
+static HANDLE hStdInput;
+static DWORD defaultMode;
+
 void disableRawMode()
 {
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &defaultMode);
-    
-
-    cout << CLEAR_SCREEN << CURSOR_HOME << RESET << SHOW_CURSOR << flush;
+    SetConsoleMode(hStdInput, defaultMode);
+    cout << RESET << SHOW_CURSOR << CURSOR_HOME << flush;
 }
 
+void enableRawMode()
+{
+    hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(hStdInput, &defaultMode);
+    atexit(disableRawMode);
+    DWORD rawMode = defaultMode;
+    rawMode &= ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT);
+    SetConsoleMode(hStdInput, rawMode);
+}
+
+void flushInputBuffer() {
+    FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+}
+
+#else
+
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static struct termios defaultMode;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &defaultMode);
+    cout << RESET << SHOW_CURSOR << CURSOR_HOME << flush;
+}
 
 void enableRawMode()
 {
@@ -29,4 +89,10 @@ void enableRawMode()
     rawMode.c_cc[VTIME] = 1;
     tcsetattr(STDIN_FILENO, TCSANOW, &rawMode);
 }
+
+void flushInputBuffer() {
+    tcflush(STDIN_FILENO, TCIFLUSH);
+}
+
+#endif
 
